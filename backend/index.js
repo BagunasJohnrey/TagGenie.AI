b168fe8ec4ffbbea2f9b4b0aeae555de66bf7d84
BagunasJohnrey <johnreybagunas2@gmail.com>
2025-11-13 16:15:09 +0800

    fix backend path vercel

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://tag-genie-mpituchde-bagunasjohnreys-projects.vercel.app",
     credentials: true
 }));
 
